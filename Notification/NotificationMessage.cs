69ec69aca9a08c7c42144893236e1b06af95cc6d
Shriram Navaratnalingam <miluka.desilva@kronos.com>
2020-04-06 18:38:40 +0530

    Added Message object and Message Handler

---
+++ Notification/NotificationMessage.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mediatr_App.Notification
+{
+    //This is the Message object that is transfered inbetween clients
+    public class NotificationMessage : INotification
+    {
+        public string NotifyText { get; set; }
+    }
+}
---
+++ Notification/Notifier1.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Mediatr_App.Notification
+{
+    //This is the Handler for the Messages
+    public class Notifier1 : INotificationHandler<NotificationMessage>
+    {
+        public Task Handle(NotificationMessage notification, CancellationToken cancellationToken)
+        {
+            Debug.WriteLine($"Debugging from Notifier 1. Message  : {notification.NotifyText} ");
+            return Task.CompletedTask;
+        }
+    }
+}
